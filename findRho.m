f32355e2c722ee9dd74baba5196b96779750a50a
suseongkim <suseongkim@snu.ac.kr>
2017-06-07 00:40:59 +0200

    fixing .....

--- findRho.m
+++ findRho.m
@@ -1,18 +1,17 @@
-function [rho,sOut,p_,solProblem] = findRho(dt,rhoInit,A,Lc1,Lc3,initRegion,Kp,Kd)
+function [rho,sOut,p_,solProblem] = findRho(dt,A,Lc1,Lc3,initRegion,Kp,Kd)
 
 checkDependency('yalmip');
 monomialOrder = 2;
 Er = 0.1;
-max_ar = 0;
+max_ar = 5;
 
-N = size(Lc1,2);
-rho = sdpvar(N+1,1);
+N = size(Lc1,2)+1;
+rho = sdpvar(N,1);
 e = sdpvar(6,1);
 epbar = sdpvar(1,1);
 edbar = sdpvar(1,1);
 epep = e(1:3)'*e(1:3);
 eded = e(4:6)'*e(4:6);
-assign(rho(1),rhoInit);
 maxKp = max(max(Kp));
 maxKd = max(max(Kd));
 
@@ -25,7 +24,7 @@ P = [p(1)   0    0  p(3)   0    0;
        0  p(3)   0    0  p(5)   0;
        0    0  p(4)   0    0  p(6)];
 
-maxPpv = p(4); maxPv = p(6); % need to fix
+maxPpv = 0; maxPv = p(5); % need to fix
 
 p = p_(7:12);
 Pnext = [p(1)   0    0  p(3)   0    0;
@@ -53,13 +52,23 @@ k=1;
 S = sdpvar(6,6);
 
 [L1,coeff1] = polynomial([e;epbar;edbar],monomialOrder);
-[L2,coeff2] = polynomial(e,monomialOrder);
-[L3,coeff3] = polynomial(e,monomialOrder);
+[L2,coeff2] = polynomial([e;epbar;edbar],monomialOrder);
+[L3,coeff3] = polynomial([e;epbar;edbar],monomialOrder);
 
 [L4,coeff4] = polynomial([e;epbar;edbar],monomialOrder);
 [L5,coeff5] = polynomial([e;epbar;edbar],monomialOrder);
 [L6,coeff6] = polynomial([e;epbar;edbar],monomialOrder);
 [L7,coeff7] = polynomial([e;epbar;edbar],monomialOrder);
+
+[L8,coeff8] = polynomial([e;epbar;edbar],monomialOrder);
+[L9,coeff9] = polynomial([e;epbar;edbar],monomialOrder);
+[L10,coeff10] = polynomial([e;epbar;edbar],monomialOrder);
+[L11,coeff11] = polynomial([e;epbar;edbar],monomialOrder);
+
+[L12,coeff12] = polynomial([e;epbar;edbar],monomialOrder);
+[L13,coeff13] = polynomial([e;epbar;edbar],monomialOrder);
+[L14,coeff14] = polynomial([e;epbar;edbar],monomialOrder);
+[L15,coeff15] = polynomial([e;epbar;edbar],monomialOrder);
 
 rhodot(k) = (rho(k+1) - rho(k))/dt;
 
@@ -72,28 +81,48 @@ c1 = sos(rhodot(k) - Vdot ...
          - L5*(edbar^2 - eded) ...
          - L6*(edbar) ...
          - L7*(epbar));
-c2 = sos(rho(k) - V - L2*(1-e'*initRegion*e));
+c2 = sos(rho(k) - V ...
+         - L2*(1-e'*initRegion*e) ...
+         - L8*(epbar^2 - epep) ...
+         - L9*(edbar^2 - eded) ...
+         - L10*(edbar) ...
+         - L11*(epbar));
 c3 = sos(L2);
-c4 = sos(1 - e'*S*e - L3*(rho(k) - V));
+c4 = sos(1 - e'*S*e ...
+         - L3*(rho(k) - V) ...
+         - L12*(epbar^2 - epep) ...
+         - L13*(edbar^2 - eded) ...
+         - L14*(edbar) ...
+         - L15*(epbar));
 c5 = sos(L6);
 c6 = sos(L7);
+c8 = sos(L10);
+c9 = sos(L11);
+c10 = sos(L14);
+c11 = sos(L15);
 c7 = S >= 0;
-constraints = [constraints c1 c2 c3 c4 c5 c6 c7];
+
+constraints = [constraints c1 c2 c3 c4 c5 c6 c7 c8 c9 c10 c11];
 cost = cost + geomean(S);
 
-vars = [vars;coeff2;coeff4;coeff5;coeff6;coeff7];
+vars = [vars;coeff2;coeff4;coeff5;coeff6;coeff7;coeff8;coeff9;coeff10;coeff11;coeff12;coeff13;coeff14;coeff15];
 sVars = [sVars;S(:)];
 sOut = [sOut S(:)];
 
 for k=2:N-1
     [L1,coeff1] = polynomial([e;epbar;edbar],monomialOrder);
-    [L3,coeff3] = polynomial(e,monomialOrder);
+    [L3,coeff3] = polynomial([e;epbar;edbar],monomialOrder);
 
     [L4,coeff4] = polynomial([e;epbar;edbar],monomialOrder);
     [L5,coeff5] = polynomial([e;epbar;edbar],monomialOrder);
     [L6,coeff6] = polynomial([e;epbar;edbar],monomialOrder);
     [L7,coeff7] = polynomial([e;epbar;edbar],monomialOrder);
-
+    
+    [L8,coeff8] = polynomial([e;epbar;edbar],monomialOrder);
+    [L9,coeff9] = polynomial([e;epbar;edbar],monomialOrder);
+    [L10,coeff10] = polynomial([e;epbar;edbar],monomialOrder);
+    [L11,coeff11] = polynomial([e;epbar;edbar],monomialOrder);
+    
     rhodot(k) = (rho(k+1) - rho(k))/dt;
 
     L1 = replace(L1,coeff1,value(Lc1(:,k)));
@@ -107,6 +136,7 @@ for k=2:N-1
          p(3)   0    0  p(5)   0    0;
            0  p(3)   0    0  p(5)   0;
            0    0  p(4)   0    0  p(6)];
+    maxPpv = p(3); maxPv = p(5); % need to fix    
 
     p = p_(6*k+1:6*(k+1));
     Pnext = [p(1)   0    0  p(3)   0    0;
@@ -116,10 +146,6 @@ for k=2:N-1
                0  p(3)   0    0  p(5)   0;
                0    0  p(4)   0    0  p(6)];
     S = sdpvar(6,6);
-    sVars = [sVars;coeff4;coeff5;coeff6;coeff7;S(:)];
-    sOut = [sOut S(:)];
-%     pVars = [pVars;p];
-        
     V = e'*P*e;
     Vdot = e'*(A'*P + P*A)*e ...
            + Er*(maxPpv*epbar+maxPv*edbar)*(maxKp*epbar+maxKd*edbar + 9.8 + max_ar) ...
@@ -131,12 +157,22 @@ for k=2:N-1
              - L5*(edbar^2 - eded) ...
              - L6*(edbar) ...
              - L7*(epbar));    
-    c4 = sos(1 - e'*S*e - L3*(rho(k) - V)); 
+    c4 = sos(1 - e'*S*e ...
+             - L3*(rho(k) - V) ... 
+             - L8*(epbar^2 - epep) ...
+             - L9*(edbar^2 - eded) ...
+             - L10*(edbar) ...
+             - L11*(epbar));
     c5 = sos(L6);
     c6 = sos(L7);
+    c8 = sos(L10);
+    c9 = sos(L11);
     c7 = S >= 0;
 
-    constraints = [constraints c1 c4 c5 c6 c7];
+    constraints = [constraints c1 c4 c5 c6 c7 c8 c9];
+    sVars = [sVars;coeff4;coeff5;coeff6;coeff7;coeff8;coeff9;coeff10;coeff11;S(:)];
+    sOut = [sOut S(:)];
+    
     cost = cost + geomean(S);
 end
 
--- freenote144.m
+++ freenote144.m
@@ -7,8 +7,9 @@ checkDependency('yalmip');
 e = sdpvar(6,1);
 dt = 0.005;
 
-initRegion = diag(1./[0.2 0.2 0.2 0.2 0.2 0.2].^2);
-max_ar = 0;
+initRegion = diag(1./[0.1 0.1 0.2 0.2 0.2 0.3].^2);
+Er = 0.1;
+max_ar = 5;
 
 Kp = diag([10 10 15]);
 Kd = diag([4 4 6]);
@@ -24,8 +25,6 @@ maxPv = max(max(P(4:6,4:6)));
 maxKp = max(max(Kp));
 maxKd = max(max(Kd));
 
-Er = 0.1;
-
 rho = sdpvar(1,1);
 rhodot = sdpvar(1,1);
 epbar = sdpvar(1,1);
@@ -34,7 +33,6 @@ edbar = sdpvar(1,1);
 epep = e(1:3)'*e(1:3);
 eded = e(4:6)'*e(4:6);
 V = e'*P*e;
-
 Vdot = e'*(P*A+A'*P)*e ...
        + Er*(maxPpv*epbar+maxPv*edbar)*(maxKp*epbar+maxKd*edbar + 9.8 + max_ar);
 
@@ -42,14 +40,17 @@ monomialOrder = 2;
 [L_init,coeff_init] = polynomial(e,monomialOrder);
 
 c1 = sos(L_init);
-c2 = sos(rho - V - L_init*(1-e'*initRegion*e));
+c2 = sos(rho - V ...
+         - L_init*(1-e'*initRegion*e));
 
 sol = solvesos([c1 c2],rho,[],coeff_init);
 rhoInit = value(rho);
 
-N = 20;
+N = 10;
 rhoTemp = rhoInit;
 rhoCont = rhoInit;
+
+% rhoCont = rhoInit*ones(N,1);
 
 for i=1:N
     [L1,coeff1] = polynomial([e;epbar;edbar],monomialOrder);
@@ -79,10 +80,40 @@ end
 ts = linspace(0,dt*(N-1),N);
 rhodot = diff(rhoCont)/dt;
 
-[coeffL1,coeffL3,S_] = findL(P,P*A +A'*P,rhoCont,rhodot,initRegion,Kp,Kd);
+P_temp = [];
+Q_temp = [];
+
+for k = 1:N+1
+    P_temp{k} = P;
+    Q_temp{k} = P*A + A'*P;
+end
+
+[coeffL1,coeffL3,S_] = findL(dt,P_temp,Q_temp,rhoCont,rhodot,initRegion,Kp,Kd);
 
 %%
-[rho,sVars,p,solProblem] = findRho(dt,rhoInit,A,coeffL1,coeffL3,initRegion,Kp,Kd);
+[rho,sVars,p,solProblem] = findRho(dt,A,coeffL1,coeffL3,initRegion,Kp,Kd);
+
+%%
+P_temp = []; Q_temp = [];
+for k = 1:N-1
+   p_temp = p(6*(k-1)+1:6*k);
+   p_temp = clean(p_temp,1e-4);
+   P_temp{k} = [p_temp(1)        0         0  p_temp(3)        0         0;
+                       0  p_temp(1)        0         0  p_temp(3)        0;
+                       0         0  p_temp(2)        0         0  p_temp(4);
+                p_temp(3)        0         0  p_temp(5)        0         0;
+                       0  p_temp(3)        0         0  p_temp(5)        0;
+                       0         0  p_temp(4)        0         0  p_temp(6)];
+                   
+   Q_temp{k} = P_temp{k}*A +A'*P_temp{k};
+end
+
+%%
+ts = linspace(0,dt*(N-1),N);
+rhodot = diff(rho(1:N-1))/dt;
+rhodot(1) = rhodot(1);
+
+[coeffL1,coeffL3,S_] = findL(dt,P_temp,Q_temp,rho(1:N-1),rhodot,initRegion,Kp,Kd);
 
 %%
 % Kp = diag([10 10 15]);
@@ -94,7 +125,8 @@ for jj = 1:N-1
     figure(101);clf;
     hold on
     P = reshape(double(sVars(:,jj)),6,6);
-    kk = 3;
+%     P = reshape(double(S_(:,jj)),6,6);
+    kk = 1;
     p1 = [P(kk,kk) P(kk,kk+3);P(kk+3,kk) P(kk+3,kk+3)];
     invp1 = inv(sqrtm(p1));
     p2 = [initRegion(kk,kk) initRegion(kk,kk+3);initRegion(kk+3,kk) initRegion(kk+3,kk+3)];
@@ -109,12 +141,4 @@ end
    axis equal
    pause(0.1);
 end
-
-
-
-
-
-
-
-
 
